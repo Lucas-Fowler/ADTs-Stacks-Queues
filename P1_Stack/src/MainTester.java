437934516c4c45b17e6723df3fbe3b1678f2edf1
1935539 <1935539@01-19956.pusd.dom>
2023-03-13 08:45:34 -0700

    Stack project and testing stuff

---
+++ P1_Stack/src/MainTester.java
@@ -0,0 +1,62 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTester {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	void testSize() {
+		//fail("Not yet implemented");
+		Stack<Integer> temp = new Stack<Integer>();
+		
+		//assertEquals(expected, actual)
+		assertEquals(0, temp.size());
+	}
+
+	
+	@Test
+	void testPush() {
+		Stack<Integer> temp = new Stack<Integer>();
+		temp.push(2);
+	}
+	
+	
+	@Test
+	void testPop() {
+		Stack<Integer> temp = new Stack<Integer>();
+		// test when the Stack is empty
+		assertEquals(null, temp.pop());
+		
+		temp.push(2);
+		temp.push(3);
+		//test when there are elements in Stack
+		assertEquals(3, temp.pop());	
+	}
+	
+	
+	@Test
+	void testEmpty() {
+		Stack<Integer> temp = new Stack<Integer>();
+		
+		assertEquals(true, temp.empty());
+	}
+	
+	
+	@Test
+	void testPeek() {
+		Stack<Integer> temp = new Stack<Integer>();
+		temp.push(2);
+		temp.push(7);
+		temp.push(6);
+		temp.push(1);
+		temp.push(23);
+		assertEquals(23, temp.peek());
+	}
+	
+}
